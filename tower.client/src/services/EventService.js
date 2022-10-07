753c93810f077b219aedba9c2fe323c219d51e6d
Randyhall91 <randyrocks11@gmail.com>
2022-10-07 11:00:46 -0600

    /.

--- tower.client/src/services/EventService.js
+++ tower.client/src/services/EventService.js
@@ -6,7 +6,7 @@ class EventService {
 
   async getEvents() {
     const res = await api.get('api/events')
-    console.log('events', res.data);
+    // console.log('events', res.data);
     AppState.towerEvent = res.data.map(e => new TowerEvent(e))
   }
   async getEventById(id) {
@@ -19,7 +19,7 @@ class EventService {
   async createEvent(formData) {
     const res = await api.post('api/events', formData)
     const newEvent = new TowerEvent(res.data)
-    console.log(res.data);
+    // console.log(res.data);
 
     AppState.towerEvent.push(newEvent)
     return res.data
@@ -43,22 +43,47 @@ class EventService {
   }
   async getTicketHolders(eventId) {
     const res = await api.get(`api/events/${eventId}/tickets`)
-    console.log('getTicketHolders', res.data)
+    // console.log('getTicketHolders', res.data)
     let ticketHolders = res.data
-    let userTicket = ticketHolders.filter(t => t.accountId == AppState.user.id)
+    let userTicket = ticketHolders.find(t => t.accountId == AppState.user.id)
     // console.log('check', checkForId.length);
     // TODO finish getting active users ticket into appstate properly
     if (userTicket) {
       AppState.usersTicket = userTicket
-      console.log('AppState.hasTicket', AppState.usersTicket);
+      // console.log('AppState.usersTicket', AppState.usersTicket.id);
+    } else {
+      AppState.usersTicket = null
+      // console.log('UsersTicketShouldbeBlank', AppState.usersTicket);
     }
     AppState.eventTicketHolders = res.data
   }
-  async removeTicket(id) {
-    await api.delete(`api/tickets/${id}`)
-    this.getTicketHolders(eventId)
+  async removeTicket() {
+
+    let ticket = AppState.usersTicket
+    // console.log('usersTicket', AppState.usersTicket);
+    await api.delete(`api/tickets/${ticket.id}`)
+    // this.getTicketHolders()
+    this.getTicketHolders(ticket.eventId)
+    AppState.usersTicket = null
+    AppState.activeEvent.capacity++
+  }
+
+
+
+  async getCommentsByEventId(eventId) {
+    const res = await api.get(`api/events/${eventId}/comments`)
+    AppState.eventComments = res.data
+    console.log('comments', res.data);
+  }
+  async removeComment(id) {
+    const res = await api.delete(`api/comments/${id}`)
+    AppState.eventComments = res.data
+  }
+  async createComment(formData) {
+    const res = await api.post('api/comments/')
 
   }
+
 
 }
 export const eventService = new EventService()
